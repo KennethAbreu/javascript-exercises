5049fcce8cd1b1484bde3998a7105e6f66c12974
Ken <kennethabreu1@gmail.com>
2021-12-02 15:56:13 -0500

    Completed reverse string exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
 const reverseString = function(str) {
+    //Split string into an array separated by a space
+    let splitString = str.split('');
+    //Reverse the array
+    let reverseArray = splitString.reverse();
+    //Combines array elements into a string
+    let joinArray = reverseArray.join('');
 
+    return joinArray
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
