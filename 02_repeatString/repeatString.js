cf4dc570cface59997a3e5979e3d790948dbc941
Ken <kennethabreu1@gmail.com>
2021-12-01 19:21:07 -0500

    Completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR'
 
+    let newString = ''
+    for (let i = 0; i < num; i++) {
+        newString += string
+    }
+    return newString
 };
+
+repeatString('hey', 10);
 
 // Do not edit below this line
 module.exports = repeatString;
